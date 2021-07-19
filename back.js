3ea98c9883c9340840097071ff00ab3b60f38f73
NovaDrake76 <novadrake76@gmail.com>
2021-07-19 19:54:27 -0300

    heroku didnt work

--- back.js
+++ back.js
@@ -1,6 +1,4 @@
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
