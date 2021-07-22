ea3527f58183336a67ea5770b367bc9a3da15673
NovaDrake76 <novadrake76@gmail.com>
2021-07-22 14:59:34 -0300

    endgame

--- back.js
+++ back.js
@@ -5,9 +5,10 @@ function sleep(ms){
 
 var x = 0;
 var a = 0;
+var incognita = false;
 
 async function number(){
-    while (x<1000000000){
+    while (x > -1){
      x++;
      x = x + a; 
      await sleep(400);
@@ -97,24 +98,105 @@ function compra7(){
     document.getElementById("breno").style.display = "inline";
     document.getElementById("guardei").style.display = "flex";
     document.getElementById("btn7").style.display = "none";
+    document.getElementById("btn6").style.display = "none";
+
     x = 0;
     a = 0;
+    incognita = true;
     var novoBotao = document.createElement("BUTTON");
     novoBotao.innerHTML = "Dupla com Hally";
     novoBotao.className = "btn btn-danger";
     novoBotao.id = "duplaHally";
     novoBotao.onclick = async function newGamePlus(){
-    
-        while(true){
+        document.getElementById("duplas").style.display = "none";
+
+
+        var litBotao = document.createElement("BUTTON");
+        litBotao.innerHTML = "Entrar no LIT";
+        litBotao.className = "btn btn-primary";
+        litBotao.id = "lit";
+        litBotao.onclick = async function lit(){
+            document.getElementById("titleHs").innerHTML = "DaniloCoins: Bad End";
+            document.getElementById("lit").style.display = "none";
+            document.getElementById("1game").style.display = "none";
+            document.getElementById("uskb").style.display = "none";
+
+            var daniloFoto = document.createElement("img");
+            daniloFoto.src="/images/danilocerejeira.webP";
+            daniloFoto.className="img-fluid";
+
+            document.getElementById('contImages').appendChild(daniloFoto);
+            var voz = document.createElement("img");
+            voz.src="/images/voz.webP";
+            voz.className="img-fluid";
+
+            document.getElementById('contImages').appendChild(voz);
+
+            x = -1;
+            while(x < 0){
+                x = x-1;
+                await sleep(400);
+            }
+        }
+        var uskbtn = document.createElement("BUTTON");
+        uskbtn.innerHTML = "Reunião com uskb";
+        uskbtn.className = "btn btn-primary mt-2";
+        uskbtn.id = "uskb";
+        uskbtn.onclick = async function uskb(){
+            document.getElementById("uskb").style.display = "none";
+            document.getElementById("1game").style.display = "none";
+            document.getElementById("lit").style.display = "none";
+
+            var pressao = document.createElement("img");
+            pressao.src="/images/pressao.webP";
+            var ht = document.createElement("img");
+            ht.src="/images/ht.webP";
+            ht.className="img-fluid";
+
+            var alce2 = document.createElement("img");
+            alce2.src="/images/alce2.webP";
+            ht.className="img-fluid";
+
+            var brn = document.createElement("img");
+            brn.src="/images/brn.webP";
+            brn.className="img-fluid";
+
+            var max = document.createElement("img");
+            max.src="/images/max.webP";
+            max.className="img-fluid";
+
+            var conselho = document.createElement("h3");
+            conselho.style.color = "white";
+            conselho.innerHTML="The council has decided to sell all the HallyCoins to start a ";
+            var projeto = document.createElement("a");
+            projeto.href = "https://www.youtube.com/watch?v=1wSoVo6zBa4";
+            projeto.target= "_blank";
+            projeto.innerHTML="new project";
+            projeto.style.fontSize ="22px";
+
+            document.getElementById('compras').appendChild(conselho);
+            document.getElementById('compras').appendChild(projeto);
+            document.getElementById('contImages').appendChild(pressao);
+            document.getElementById('contImages').appendChild(ht);
+            document.getElementById('contImages').appendChild(alce2);
+            document.getElementById('contImages').appendChild(brn);
+            document.getElementById('contImages').appendChild(max);
+            
+            }
+
+        document.getElementById("compras").appendChild(litBotao);
+        document.getElementById("compras").appendChild(uskbtn);
+
+        while(incognita = true){
             x = Math.random()*1000;
             await sleep(400);
         }
+
     }
 
     document.getElementById("duplas").appendChild(novoBotao);
 
 }
-
 
 function graph(){
 
@@ -158,7 +240,8 @@ setInterval(news, 15000);
         'Daí o médico te fala: pela manhã, uma xícara de café e um tapa em hally. Melhor obedecer o doutor',
         'Ari tem um server de minecraft secreto, só perguntar pelo botão de fazer cocô que ele passa o IP',
         'https://github.com/laynH/Anime-Girls-Holding-Programming-Books',
-        'O disco voador cruzou o corredor'
+        'O disco voador cruzou o corredor',
+        'Ah se tivesse mais Tchonga, o mundo seria melhor...'
         
     ];
     var randomNumber = Math.floor(Math.random()*textArray.length);
